dee4f6964d64ff5470e6444f7f68ddd296fc209b
Aleksandr Petrov <vorked@mail.ru>
2024-09-11 13:59:50 +0300

    added example operations

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-  //      System.out.println("Случайное число от 0 до 1: " + Math.random());
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "  + text.length());
+        System.out.println("Введите 1-е число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите 2-е число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("сумма: " + (firstNumber + secondNumber) );
+        System.out.println("разность: " + (firstNumber - secondNumber) );
+        System.out.println("произведение: " + (firstNumber * secondNumber) );
+        double division = (double)firstNumber/secondNumber;
+        System.out.println("частное : " + division );
+
 
     }
 }
